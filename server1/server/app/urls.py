eeea76c105773624c35217cefcfe71a02e56a398
Sofya <s.cheboltasova@g.nsu.ru>
2024-03-07 18:13:40 +0700

    server/refactor: remove commebts

--- server1/server/app/urls.py
+++ server1/server/app/urls.py
@@ -1,14 +1,3 @@
-# from django.urls import path, include
-# from django.contrib import admin
-# from django.views.generic import TemplateView
-# from .views import index
-
-# urlpatterns = [
-#     # path('', index),
-#     path('api/', include('api.urls')), 
-#     path('admin/', admin.site.urls),
-# ]
-
 from django.urls import path
 from . import views
 
