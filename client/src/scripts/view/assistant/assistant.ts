188be708b4014c51a9a9e525ac2789d5c06a8e7d
Sofya <s.cheboltasova@g.nsu.ru>
2024-03-31 22:48:25 +0700

    client/feat: add html and styles for messages

--- client/src/scripts/view/assistant/assistant.ts
+++ client/src/scripts/view/assistant/assistant.ts
@@ -2,12 +2,13 @@ import EventObserver from "../../observer/observer";
 
 class Assistant {
   public tag: HTMLDivElement;
-  private textArea: HTMLTextAreaElement;
+  private dialog: HTMLDivElement;
   private eventObserver: EventObserver;
 
   constructor(eventObserver: EventObserver) {
     this.tag = document.createElement("div");
-    this.textArea = document.createElement("textarea");
+    this.dialog = document.createElement("div");
+
     this.eventObserver = eventObserver;
     this.eventObserver.subscribe(
       "assistantAnswer",
@@ -19,8 +20,8 @@ class Assistant {
     this.tag = document.createElement("div");
     this.tag.classList.add("assistant__wrapper");
 
-    const dialog = document.createElement("div");
-    dialog.classList.add("assistant__dialog");
+    this.dialog = document.createElement("div");
+    this.dialog.classList.add("assistant__dialog");
 
     const form = document.createElement("form");
     form.classList.add("assistant__form");
@@ -30,24 +31,43 @@ class Assistant {
     textArea.addEventListener("keydown", this.handleEnterPress.bind(this));
 
     form.appendChild(textArea);
-    this.tag.append(dialog, form);
+    this.tag.append(this.dialog, form);
+  }
+
+  private setUserMessage(inputText: string) {
+    const message = document.createElement("div");
+    message.classList.add("message__user");
+    message.textContent = inputText;
+    this.dialog.appendChild(message);
   }
 
   private handleEnterPress(event: KeyboardEvent): void {
     if (event.key === "Enter") {
       const inputText = (event.target as HTMLTextAreaElement).value;
+
+      this.setUserMessage(inputText);
+
       this.eventObserver.notify("enterPressed", inputText);
       (event.target as HTMLTextAreaElement).value = "";
     }
   }
 
+  private setAssistantMessage(inputText: string) {
+    const message = document.createElement("div");
+    message.classList.add("message__assistant");
+    message.textContent = inputText;
+    this.dialog.appendChild(message);
+  }
+
   private handleApiCall(data: string): void {
     console.error("Данные из API:", data);
 
-    const editData = data
+    const editedMessage = data
       .replace(/\n/g, "<br>")
       .replace(/\*\*(.*?)\*\*/g, "<b>$1</b>")
       .replace(/\-/g, "	•");
+
+    this.setAssistantMessage(editedMessage);
   }
 }
 
