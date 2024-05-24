99a767a565dfe9cad205be31d7a72a039675bde9
Sofya <s.cheboltasova@g.nsu.ru>
2024-05-24 14:01:13 +0700

    client/refactor: change types

--- client/src/scripts/observer/observer.ts
+++ client/src/scripts/observer/observer.ts
@@ -1,7 +1,9 @@
+import { EventTypes } from "../contracts/enums";
+
 class EventObserver {
   private observers: { [eventType: string]: ((data: any) => void)[] } = {};
 
-  public subscribe(eventType: string, observer: (data: any) => void): void {
+  public subscribe(eventType: EventTypes, observer: (data: any) => void): void {
     if (!this.observers[eventType]) {
       this.observers[eventType] = [];
     }
@@ -16,7 +18,7 @@ class EventObserver {
     }
   }
 
-  public notify(eventType: string, data: any): void {
+  public notify(eventType: EventTypes, data: any): void {
     if (this.observers[eventType]) {
       this.observers[eventType].forEach((observer) => observer(data));
     }
