143bd0a8508114dd79bf83544c71dbbb4a98425c
Sofya <s.cheboltasova@g.nsu.ru>
2024-05-24 14:02:57 +0700

    client/feat: configure links

---
+++ client/src/scripts/contracts/enums.ts
@@ -0,0 +1,20 @@
+enum EventTypes {
+  ENTER_PRESSED,
+  URL_CHANGED,
+  ASSISTANT_ANSWER,
+}
+
+enum Links {
+  home = "home",
+  assistant = "assistant",
+  knowledgeBase = "knowledge-base",
+  banks = "banks",
+  products = "products",
+  categories = "categories",
+  category_subsections = "category_subsections",
+  subsection_details = "subsection_details",
+  detailed_data = "detailed_data",
+}
+
+export { EventTypes, Links };
+
