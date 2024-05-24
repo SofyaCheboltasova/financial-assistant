a50222ff44a98a7d581a28d94217835ae1f9f62f
Sofya <s.cheboltasova@g.nsu.ru>
2024-05-24 14:07:12 +0700

    client/feat: change app's entry point

--- client/src/scripts/app/app.ts
+++ client/src/scripts/app/app.ts
@@ -1,98 +1,31 @@
-import { ResponseType, Categories, TableData } from "../contracts/interfaces";
+import { EventTypes } from "../contracts/enums";
 import AppController from "../controller/controller";
 import EventObserver from "../observer/observer";
-import AppView from "../view/appView";
+import Router from "./route";
 
 class App {
   private controller: AppController;
-  private view: AppView;
-  private eventObserver: EventObserver;
-
-  public url: URL;
-  public currentPage: string;
+  private observer: EventObserver;
+  private router: Router;
 
   constructor() {
-    this.eventObserver = new EventObserver();
+    this.router = new Router();
+    this.observer = new EventObserver();
+    this.controller = new AppController(this.observer, this.router);
 
-    this.controller = new AppController(this.eventObserver);
-    this.view = new AppView(this.eventObserver);
-
-    this.url = new URL("http://localhost:8080/");
-    this.currentPage = this.url.hash;
+    window.addEventListener("popstate", () => this.router.resolveRoute());
+    window.addEventListener("hashchange", () => this.router.resolveRoute());
   }
 
   public start(): void {
-    this.setUrlChangeListener();
-    this.view.setHomePage();
-  }
+    this.observer.subscribe(
+      EventTypes.URL_CHANGED,
+      this.router.navigate.bind(this.router)
+    );
 
-  private getCurrentPage(): string {
-    const url = window.location.href;
-    const parts = url.split("#");
-    return parts[parts.length - 1];
-  }
-
-  private setUrlChangeListener() {
-    window.addEventListener("popstate", () => {
-      this.currentPage = this.getCurrentPage();
-      this.changePage();
-    });
-  }
-
-  private async changePage(): Promise<void> {
-    switch (this.currentPage) {
-      case "assistant": {
-        this.view.setAssistantPage();
-        this.controller.subscribeToAssistantEvents();
-        break;
-      }
-      case "banks": {
-        const banks: ResponseType[] | [] = await this.controller.getBanks();
-        this.view.drawKnowledgeBasePage(banks);
-        this.view.setButtonHandler("Bank");
-        break;
-      }
-      case "products": {
-        const products: ResponseType[] | [] =
-          await this.controller.getProducts();
-        this.view.drawKnowledgeBasePage(products);
-        this.view.setButtonHandler("Product");
-        break;
-      }
-      case "categories": {
-        const categories: Categories[] | [] =
-          await this.controller.getProductCategories();
-        this.view.drawKnowledgeBasePage(categories);
-        this.view.setButtonHandler("Category");
-        break;
-      }
-      case "category-subsections": {
-        const subsections: ResponseType[] | [] =
-          await this.controller.getCategorySubsections();
-        this.view.drawKnowledgeBasePage(subsections);
-        this.view.setButtonHandler("Subsections");
-        break;
-      }
-      case "subsection-details": {
-        const subsectionDetails: ResponseType[] | [] =
-          await this.controller.getSubsectionDetails();
-        this.view.drawKnowledgeBaseList(subsectionDetails);
-        this.view.setButtonHandler("SubsectionDetails");
-        break;
-      }
-      case "detailed-information": {
-        const detailedInformation: TableData[] | [] =
-          await this.controller.getDetailedInformation();
-        this.view.drawKnowledgeBaseTable(detailedInformation);
-        break;
-      }
-      default: {
-        this.view.setHomePage();
-        break;
-      }
-    }
+    const path = this.router.getPath();
+    this.router.navigate(path);
   }
 }
 
 export default App;
-
