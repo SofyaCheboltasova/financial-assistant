7a727a527999975d0329c78efbd7c40d96b3ee21
Sofya <s.cheboltasova@g.nsu.ru>
2024-05-24 14:05:42 +0700

    client/refactor: update url change

--- client/src/scripts/view/home/home.ts
+++ client/src/scripts/view/home/home.ts
@@ -1,23 +1,38 @@
+import { EventTypes, Links } from "../../contracts/enums";
+import EventObserver from "../../observer/observer";
+
 class Home {
   public main: HTMLElement;
   public tag: HTMLDivElement | undefined;
+  private observer: EventObserver;
 
-  constructor() {
+  constructor(observer: EventObserver) {
+    this.observer = observer;
     this.main = document.createElement("main");
     this.main.classList.add("main");
 
     document.body.appendChild(this.main);
   }
 
-  public drawHomePage(): void {
+  private setButtonListener(a: HTMLAnchorElement, path: string): void {
+    a.addEventListener("click", (e) => {
+      e.preventDefault();
+      const url = new URL(location.toString());
+      url.pathname = path;
+      history.pushState(null, "", url.toString());
+      this.observer.notify(EventTypes.URL_CHANGED, path);
+    });
+  }
+
+  public renderStartPage(): void {
     this.tag = document.createElement("div");
     this.tag.classList.add("main__wrapper");
 
     const baseLink = document.createElement("a");
-    baseLink.href = "#banks";
+    this.setButtonListener(baseLink, Links.knowledgeBase);
 
     const assistantLink = document.createElement("a");
-    assistantLink.href = "#assistant";
+    this.setButtonListener(assistantLink, Links.assistant);
 
     const baseButton = document.createElement("div");
     baseButton.textContent = "База знаний";
