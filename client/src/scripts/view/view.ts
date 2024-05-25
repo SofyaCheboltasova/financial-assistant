574a123f26f0bb10fe860013e66ac8c8d0052a46
Sofya <s.cheboltasova@g.nsu.ru>
2024-05-25 09:23:31 +0700

    client/feat: add logic for previous page button

--- client/src/scripts/view/view.ts
+++ client/src/scripts/view/view.ts
@@ -4,23 +4,46 @@ import Assistant from "./assistant/assistant";
 import DataType from "../contracts/types";
 import { TableData } from "../contracts/interfaces";
 import EventObserver from "../observer/observer";
+import { EventTypes } from "../contracts/enums";
 class View {
   private home: Home;
   private assistant: Assistant;
+  private observer: EventObserver;
   public knowledgeBase: KnowledgeBase;
+
   private mainTag: HTMLElement;
+  public prevPageButton: HTMLElement;
 
   constructor(observer: EventObserver) {
+    this.observer = observer;
     this.home = new Home(observer);
     this.knowledgeBase = new KnowledgeBase();
-    this.assistant = new Assistant();
+    this.assistant = new Assistant(observer);
 
     this.mainTag = this.home.main;
+    this.prevPageButton = this.setPrevPageButton();
+  }
+
+  private setPrevPageButton(): HTMLElement {
+    const button = document.createElement("div");
+    button.classList.add("main__prev-page");
+    this.mainTag.appendChild(button);
+
+    button.addEventListener("click", () => {
+      this.observer.notify(EventTypes.CHANGE_PAGE);
+    });
+    return button;
+  }
+
+  private removePrevPageButton(): void {
+    const prevPageButton = document.querySelector(".main__prev-page");
+    prevPageButton?.remove();
   }
 
   public renderStartPage(): void {
     this.home.renderStartPage();
     this.updateMainTag(this.home.tag);
+    this.removePrevPageButton();
   }
 
   public renderAssistantPage(): void {
@@ -46,6 +69,7 @@ class View {
     }
 
     if (newTag) this.mainTag.appendChild(newTag);
+    this.setPrevPageButton();
   }
 }
 
